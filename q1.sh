d5c7277581abd597ab309e0478a58b1e34af7ddc
wambasisamuel <wambasisamuel@gmail.com>
2021-07-12 16:52:58 +0300

    Add files via upload

--- q1.sh
+++ q1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-users=$(awk -F ":" 'BEGIN {OFS=":"}{print $1,$6}' /etc/passwd)
-#md5_file=/var/log/current_users
-#changes_file=/var/log/user_changes
+# 0 * * * * /home/root/q1.sh > /dev/null   # Crontab entry
 
-md5_file=./current_users
-changes_file=./user_changes
+users=$(awk -F ":" 'BEGIN {OFS=":"}{print $1,$6}' /etc/passwd)
+md5_file=/var/log/current_users
+changes_file=/var/log/user_changes
+
+# Create files if they do not exist
 test -f $md5_file || touch $md5_file
 test -f $changes_file || touch $changes_file
 
 oldhash=$(cat $md5_file | awk -F " " '{print $1}')
 newhash=$(echo -n $users | md5sum | awk -F " " '{print $1}')
 
-echo $oldhash
-echo $newhash
+# echo $users
 
 if [ "$newhash" != "$oldhash" ]; then
 	echo "$(date "+%Y/%m/%d %T") changes occurred" > $changes_file
